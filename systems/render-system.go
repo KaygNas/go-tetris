bee94f08d8175a70ec872843e4eda5d809661b15
huangchengjie <597857074@qq.com>
2024-07-21 22:21:21 +0800

    feat: install github.com/nsf/termbox-go

--- systems/render-system.go
+++ systems/render-system.go
@@ -2,6 +2,9 @@ package systems
 
 import (
 	"go-tetris/entities"
+	"time"
+
+	"github.com/nsf/termbox-go"
 )
 
 type IRenderSystem interface {
@@ -9,19 +12,26 @@ type IRenderSystem interface {
 	render(*entities.Game)
 }
 
-type RenderSystem struct{}
+type RenderSystem struct {
+	game *entities.Game
+}
 
 func (rs *RenderSystem) Init(g *entities.Game) {
-
+	termbox.Init()
+	rs.game = g
 }
 func (rs *RenderSystem) Tick() {
-
+	rs.render(rs.game)
 }
 func (rs *RenderSystem) Close() {
-
+	termbox.Close()
 }
 func (rs *RenderSystem) render(g *entities.Game) {
-
+	now := time.Now().String()
+	for i, c := range now {
+		termbox.SetCell(0+i, 0, c, termbox.ColorWhite, termbox.ColorBlack)
+	}
+	termbox.Flush()
 }
 
 func NewRenderSystem() IRenderSystem {
